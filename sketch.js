a19d83d1fc74728acece0e95bceb56b3d3880c03
SamT6 <SamT6@users.noreply.github.com>
2017-03-20 11:58:08 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+	createCanvas(400, 400);
+	console.log("setup function");
+}
+
+function draw() {
+  background(255,0 , 0);
+  ellipse(200, 200, 50, 50);
+	console.log("draw function");
+}
